0acede16a82965dab8203c1acd7e0dcba3a77af3
MrTibbits <102533894+MrTibbits@users.noreply.github.com>
2022-06-25 10:55:08 +0300

    SJ: First Submission
    The initial Github commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Project_BlueEpoch
+
+Developed with Unreal Engine 5
---
+++ Source/Project_BlueEpoch/Project_BlueEpoch.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Project_BlueEpoch.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Project_BlueEpoch, "Project_BlueEpoch" );
